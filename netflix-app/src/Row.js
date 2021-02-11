0d80618c2b2391da41d90cda8bff2f40d54d4847
Paul Morales <paulginga@hotmail.com>
2021-02-11 17:32:53 -0600

    added async function that waits for results before executing within useEffect function

--- netflix-app/src/Row.js
+++ netflix-app/src/Row.js
@@ -1,14 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
 const [movies, setMovies] = useState([]);
 
 useEffect(() => {
-    return () => {
-      
+    async function fetchData(){
+        const request = await axios.get(fetchUrl);
+        console.log(request)
+        return request
     }
-}, [movies])
+    fetchData();
+}, [])
 
     return (
         <div>
