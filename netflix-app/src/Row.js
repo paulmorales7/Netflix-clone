1fb5e07a427a39f3804c3436d0509fafdbdc2817
Paul Morales <paulginga@hotmail.com>
2021-02-12 12:21:30 -0600

    working on fixing the api images not showing issue

--- netflix-app/src/Row.js
+++ netflix-app/src/Row.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function Row({ title, fetchUrl }) {
-const [movies, setMovies] = useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-useEffect(() => {
-    async function fetchData(){
-        const request = await axios.get(fetchUrl);
-        setMovies(request.data.result)
-        return request
-    }
-    fetchData();
-}, [])
+function Row({ title, fetchUrl }) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results)
+            
+            return request
+        }
+        fetchData();
+    }, [fetchUrl])
+
+    console.log(movies)
 
     return (
-        <div>
+        <div className='row'>
             <h2>{title}</h2>
+
+            <div className='row_posters'>
+
+                {movies.map(movie => {
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                })}
+            </div>
         </div>
     )
 }
