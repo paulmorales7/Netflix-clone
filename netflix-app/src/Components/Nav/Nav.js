f0d2f3d0084e2fbb0b354ae8bc2014e7ef2276fc
Paul Morales <paulginga@hotmail.com>
2021-02-13 17:18:28 -0600

    completed stlying basic look and layout of nav component that sits above the banner component

--- netflix-app/src/Components/Nav/Nav.js
+++ netflix-app/src/Components/Nav/Nav.js
@@ -4,7 +4,7 @@ import './Nav.css'
 function Nav() {
     return (
         <div className='nav'>
-         <img className='nav_button' 
+         <img className='nav_logo' 
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
         alt="Netflix Logo"/>
 
