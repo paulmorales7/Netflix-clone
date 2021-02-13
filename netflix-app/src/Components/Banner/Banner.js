463c34cc6296dd6055cc5ed586997c39d1508706
Paul Morales <paulginga@hotmail.com>
2021-02-13 15:28:28 -0600

    adding information to populate the banner section, starting with title now then moving on to background image and description

--- netflix-app/src/Components/Banner/Banner.js
+++ netflix-app/src/Components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import requests from '../../requests';
+import './Banner.css';
 
 function Banner() {
 const [movie, setMovie] = useState([]);
@@ -18,8 +19,18 @@ useEffect(() => {
     fetchData();
   }, []);
     return (
-        <header>
-
+        <header className='banner'
+        style={{
+            backgroundSize: "cover",
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+            backdropPosition: "center center",
+        }}
+        >
+           <div className='banner_content'>
+           <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+           </div>
         </header>
     )
 }
