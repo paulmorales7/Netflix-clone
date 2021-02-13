f23afd38e6060e21c859cf2e48e3a53127f41479
Paul Morales <paulginga@hotmail.com>
2021-02-13 15:43:08 -0600

    adding more stlying for banner, using truncate function to cut off description length

--- netflix-app/src/Components/Banner/Banner.js
+++ netflix-app/src/Components/Banner/Banner.js
@@ -18,6 +18,12 @@ useEffect(() => {
     }
     fetchData();
   }, []);
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+
     return (
         <header className='banner'
         style={{
@@ -30,6 +36,13 @@ useEffect(() => {
            <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
+
+        <div className='banner_buttons'>
+<button className='banner_button'>Play</button>
+<button className='banner_button'>My List</button>
+        </div>
+<h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
+
            </div>
         </header>
     )
