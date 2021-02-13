db971952b1fae65191619677b4635e4edb8bb222
Paul Morales <paulginga@hotmail.com>
2021-02-13 15:52:37 -0600

    finished banner component, starting nav component

--- netflix-app/src/Components/Banner/Banner.js
+++ netflix-app/src/Components/Banner/Banner.js
@@ -44,6 +44,8 @@ useEffect(() => {
 <h1 className='banner_description'>{truncate(movie?.overview, 150)}</h1>
 
            </div>
+
+           <div className='banner_fade' />
         </header>
     )
 }
