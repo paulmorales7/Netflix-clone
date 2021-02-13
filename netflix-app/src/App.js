34b989311dd8f53edf3cd344346d3732684d8a67
Paul Morales <paulginga@hotmail.com>
2021-02-13 15:03:16 -0600

    added isLargeRow to row function parameters and added it to classnames to add astyling

--- netflix-app/src/App.js
+++ netflix-app/src/App.js
@@ -5,8 +5,14 @@ import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
