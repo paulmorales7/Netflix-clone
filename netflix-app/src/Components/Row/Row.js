cc07302dd9c728abf7c5fafe2ac84bff91fcadf8
Paul Morales <paulginga@hotmail.com>
2021-02-13 18:08:25 -0600

    finizalizing adding trailers to images, running into an issue with rowjs

--- netflix-app/src/Components/Row/Row.js
+++ netflix-app/src/Components/Row/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Row.css';
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer'
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
     function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("")
 
     useEffect(() => {
         async function fetchData() {
@@ -18,7 +21,25 @@ const base_url = "https://image.tmdb.org/t/p/original/";
         fetchData();
     }, [fetchUrl])
 
-    console.log(movies)
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+          autoplay: 1,
+        },
+      };
+
+      const handleClick = (movie) => {
+if(trailerUrl) {
+    setTrailerUrl('')
+} else {
+    movieTrailer(movie?.name || "")
+    .then(url => {
+const urlParams = new URLSearchParams (new URL(url)).search;
+setTrailerUrl(urlParams.get('v'));
+    }).catch(error => console.log(error))
+}
+}
 
     return (
         <div className='row'>
@@ -26,13 +47,22 @@ const base_url = "https://image.tmdb.org/t/p/original/";
 
             <div className='row_posters'>
 
-                {movies.map(movie => (
-                    <img key={movie.id} className={`row_poster ${isLargeRow && 'row_posterLarge'}`} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
-                ))}
+                {movies.map((movie) => (
+                    <img 
+                    key={movie.id}
+                    onClick={()=> handleClick(movie)} 
+                    className={`row_poster ${isLargeRow && 'row_posterLarge'}`} 
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
+                    alt={movie.name} 
+                    />
+                )
+                )}
             </div>
-        </div>
-    )
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+    );
 }
+
 
 export default Row;
 
